f715814a4f814e8b784d02ba148bb63f2e31f0e3
BiPolarScientist <nolanmichniewicz@gmail.com>
2025-08-10 18:27:43 -0400

    Add files via upload

---
+++ PS2/24497 - Rugrats: Royal Ransom (Crunch)/achievements.ts
@@ -0,0 +1,9 @@
+﻿import { define as $, ConditionBuilder, Condition } from '../node_modules/@cruncheevos/core'
+import * as data from './data.ts'
+
+function beatLevelOnToughFirstTime(levelID: number): ConditionBuilder {
+    return $('=2').with({ lvalue: data.difficulty })
+    
+}
+console.log(beatLevelOnToughFirstTime(0x5).toString())
+
