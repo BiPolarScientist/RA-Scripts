c58d0eaaa248ebcf54cd5e7be9b29dadbf8b760d
BiPolarScientist <nolanmichniewicz@gmail.com>
2025-08-10 20:36:22 -0400

    Fixing my nonsense

---
+++ CruncheevoSets/src/PS2/24497 - Rugrats Royal Ransom/achievements.ts
@@ -0,0 +1,9 @@
+﻿import { define as $, ConditionBuilder, Condition } from '@cruncheevos/core'
+import * as data from './data.js'
+
+export function beatLevelOnToughFirstTime(levelID: number): ConditionBuilder {
+    return $('0=2').withLast({ lvalue: data.difficulty.lvalue })
+    
+}
+
+
---
+++ CruncheevoSets/src/PS2/24497 - Rugrats Royal Ransom/index.ts
@@ -0,0 +1,17 @@
+﻿/*import { AchievementSet, define as $ } from '@cruncheevos/core'
+
+import makeAchievements from './achievements.ts'
+import makeLeaderboards from './leaderboards.ts'
+import makeRp from './rp.ts'
+
+const set = new AchievementSet({
+    gameId: 24497,
+    title: 'Rugrats: Royal Ransom'
+})
+
+
+makeAchievements(set)
+makeLeaderboards(set)
+export const rich = makeRp()
+
+export default set */
