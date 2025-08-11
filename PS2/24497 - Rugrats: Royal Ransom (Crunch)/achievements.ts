3563cdcb2adfd30cb980942bffb0824fa1b25d73
BiPolarScientist <nolanmichniewicz@gmail.com>
2025-08-10 20:39:01 -0400

    Delete PS2 directory

--- PS2/24497 - Rugrats: Royal Ransom (Crunch)/achievements.ts
+++
@@ -1,9 +0,0 @@
-﻿import { define as $, ConditionBuilder, Condition } from '../node_modules/@cruncheevos/core'
-import * as data from './data.ts'
-
-function beatLevelOnToughFirstTime(levelID: number): ConditionBuilder {
-    return $('=2').with({ lvalue: data.difficulty })
-    
-}
-console.log(beatLevelOnToughFirstTime(0x5).toString())
-
