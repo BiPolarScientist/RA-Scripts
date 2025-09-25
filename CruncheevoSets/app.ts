c63a32a4235859289761a73d226fd2d8a8fc98c2
BiPolarScientist <nolanmichniewicz@gmail.com>
2025-09-25 14:09:29 -0500

    ;P

--- CruncheevoSets/app.ts
+++ CruncheevoSets/app.ts
@@ -1,5 +1,9 @@
 // Ctrl + ` , then call tsc app.ts to build into a js file to be able to run
 
-import set from './src/PS2/24497 - Rugrats Royal Ransom/index'
-console.log(set.toString())
-console.log('hi')
+import { set, rich } from './src/PS2/24497 - Rugrats Royal Ransom/index'
+import * as fs from 'fs'
+
+
+
+fs.writeFileSync('./output.txt', set.toString())
+fs.writeFileSync('./richoutput.txt', rich.toString())
